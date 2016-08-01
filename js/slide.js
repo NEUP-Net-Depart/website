b046d74f697400e450461f90c82c0e2336a39c8c
onionwyl <362919284@qq.com>
2016-08-01 10:22:52 +0800

    Init

---
+++ js/slide.js
@@ -0,0 +1,13 @@
+//menu
+$(document).ready(function(){
+  
+  $('li.mainlevel').mousemove(function(){
+  $(this).find('ul').slideDown();//you can give it a speed
+  });
+  $('li.mainlevel').mouseleave(function(){
+  $(this).find('ul').slideUp("fast");
+  });
+  
+});
+
+//¿¡»ÀÕºø‚ www.lanrentuku.com
