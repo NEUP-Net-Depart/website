87e8544df94c0bdff66e2109cfc7b529f7b9ea51
onionwyl <362919284@qq.com>
2016-08-04 22:15:26 +0800

    Finish single.php

--- header.php
+++ header.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>先锋网-共青团东北大学委员会</title>
+<title>
+    <?php 
+        if(is_home())
+        {
+            bloginfo('name');
+        }
+        elseif(is_category())
+        {
+            single_cat_title();
+            echo ' - ';
+            bloginfo('name');
+        }
+        elseif(is_search())
+        {
+            echo "搜索结果 - ";
+            bloginfo('name');
+        }
+        elseif(is_404())
+        {
+            echo "页面不存在";
+        }
+        else{
+            wp_title('',true);
+        }
+    ?>
+</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="keywords" content="先锋网-共青团东北大学委员会" />
 <meta name="description" content="先锋网-共青团东北大学委员会，此处需描述" />
 <link href="<?php bloginfo('template_url'); ?>/css/style.css" rel="stylesheet" />
+<?php if(is_single()):?>
+<link href="<?php bloginfo('template_url'); ?>/css/contentpagestyle2.css" rel="stylesheet"/>
+<?php endif;?>
 <!-- 效果开始 -->
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/tabqh.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/slideshow.js"></script>
@@ -27,37 +55,37 @@
 <!-- 导航条开始 -->
 <div id="menu">
 <ul id="nav">
-    <li class="mainlevel" id="mainlevel_01"><a href="http://www.lanrentuku.com/" target="_blank">先锋首页</a>    </li>
-    <li class="mainlevel" id="mainlevel_02"><a href="http://www.lanrentuku.com/" target="_blank">东大共青团</a>
+    <li class="mainlevel" id="mainlevel_01"><a href="<?php echo get_option('home'); ?>" target="_blank">先锋首页</a>    </li>
+    <li class="mainlevel" id="mainlevel_02"><a href="#" target="_blank">东大共青团</a>
     <ul id="sub_02">
-    <li><a href="http://www.maitianquan.com/" target="_blank">团委简介</a></li>
-    <li><a href="http://www.maitianquan.com/" target="_blank">机构设置</a></li>
-    <li><a href="http://www.maitianquan.com/" target="_blank">工作职责</a></li>
-    <li><a href="http://www.maitianquan.com/" target="_blank">学校团委</a></li>
+    <li><a href="#" target="_blank">团委简介</a></li>
+    <li><a href="#" target="_blank">机构设置</a></li>
+    <li><a href="#" target="_blank">工作职责</a></li>
+    <li><a href="#" target="_blank">学校团委</a></li>
     </ul>
     </li>
-    <li class="mainlevel" id="mainlevel_03"><a href="http://www.lanrentuku.com/" target="_blank">学生组织</a>
+    <li class="mainlevel" id="mainlevel_03"><a href="#" target="_blank">学生组织</a>
     <ul id="sub_03">
-    <li><a href="http://www.lanrentuku.com/" target="_blank">学生会</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">研究生会</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">社团联合会</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">青年新媒体中心</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">素质拓展中心</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">社会实践中心</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">思想教育中心</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">“践行”志愿者协会</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">文化艺术中心</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">先锋网络中心</a></li>
+    <li><a href="#" target="_blank">学生会</a></li>
+    <li><a href="#" target="_blank">研究生会</a></li>
+    <li><a href="#" target="_blank">社团联合会</a></li>
+    <li><a href="#" target="_blank">青年新媒体中心</a></li>
+    <li><a href="#" target="_blank">素质拓展中心</a></li>
+    <li><a href="#" target="_blank">社会实践中心</a></li>
+    <li><a href="#" target="_blank">思想教育中心</a></li>
+    <li><a href="#" target="_blank">“践行”志愿者协会</a></li>
+    <li><a href="#" target="_blank">文化艺术中心</a></li>
+    <li><a href="#" target="_blank">先锋网络中心</a></li>
     </ul>
     </li>
-    <li class="mainlevel" id="mainlevel_04"><a href="http://www.lanrentuku.com/" target="_blank">网上团支部</a></li>
-    <li class="mainlevel" id="mainlevel_05"><a href="http://www.lanrentuku.com/" target="_blank">青年之声</a></li>
-    <li class="mainlevel" id="mainlevel_06"><a href="http://www.lanrentuku.com/" target="_blank">先锋论坛</a></li>
-    <li class="mainlevel" id="mainlevel_07"><a href="http://www.lanrentuku.com/" target="_blank">文件下载</a>
+    <li class="mainlevel" id="mainlevel_04"><a href="#" target="_blank">网上团支部</a></li>
+    <li class="mainlevel" id="mainlevel_05"><a href="#" target="_blank">青年之声</a></li>
+    <li class="mainlevel" id="mainlevel_06"><a href="#" target="_blank">先锋论坛</a></li>
+    <li class="mainlevel" id="mainlevel_07"><a href="#" target="_blank">文件下载</a>
     <ul id="sub_07">
-    <li><a href="http://www.lanrentuku.com/" target="_blank">上级文件</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">团发文件</a></li>
-    <li><a href="http://www.lanrentuku.com/" target="_blank">办事指南</a></li>
+    <li><a href="#" target="_blank">上级文件</a></li>
+    <li><a href="#" target="_blank">团发文件</a></li>
+    <li><a href="#" target="_blank">办事指南</a></li>
     </ul>
     </li>
      <li class="jquery_out" style="padding-left:25px;">
---
+++ single.php
@@ -0,0 +1,81 @@
+<?php get_header()?>
+<?php if(have_posts()):the_post();
+$catname = "";
+$category = get_the_category();
+$catname = $category[0]->cat_name;
+$catlink = get_category_link($category[0]->term_id );
+$catid = $category[0]->term_id;
+?>
+<table width="1280" border="0" align="center" cellpadding="0" cellspacing="0" style="padding-bottom: 12px">
+    <tr>
+        <!--左一表格 -->
+        <!--焦点图开始 -->
+        <td class="left" width="140" valign="top">
+        </td>
+        <!--左一结束 -->
+        <!--中间表格开始-->
+        <td valign="top">
+            <table class="middle" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td  height="26" style=";padding-bottom: 15px;">
+                        <div class="position" align="left">当前位置: <a href="<?php echo get_option('home'); ?>">首页</a> >> <a href="<?php echo $catlink?>"><?php echo $catname;?></a></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table border="0" cellspacing="0" cellpadding="0" style="padding-bottom: 15px">
+                            <tr>
+                                <td height="1" width="832"
+                                    style="background-image: url(<?php bloginfo('template_url'); ?>/images/listpageimages/line1.png);background-size: 832px 1px;background-repeat: no-repeat;"></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div align="center"><p class="title"><?php the_title(); ?></p></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div align="center"><p class="origin">发表时间:<?php the_time('Y年n月j日') ?> 文章来源:<?php the_author(); ?> 浏览次数:<?php the_views(); ?></p></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                       <div align="left"><p class="main_body"><?php the_content(); ?></p></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table border="0" cellspacing="0" cellpadding="0" style="padding-bottom: 15px;padding-top: 20px">
+                            <tr>
+                                <td height="1" width="832"
+                                    style="background-image: url(<?php bloginfo('template_url'); ?>/images/listpageimages/line1.png);background-size: 832px 1px;background-repeat: no-repeat;"></td>
+                            </tr>
+
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p class="more"><?php if (get_next_post($catid)) { next_post_link('上一篇: %link','%title',true);} else { echo "已是最新文章";} ?></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p class="more"><?php if (get_previous_post($catid)) { previous_post_link('下一篇: %link','%title',true);} else { echo "已是最后文章";} ?></p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+        <!--中间表格结束-->
+        <!--右侧表格开始-->
+        <td class="right" width="140" valign="top">
+
+        </td>
+        <!--右侧表格结束-->
+    </tr>
+</table>
+<?php endif;?>
+<?php get_footer()?>
