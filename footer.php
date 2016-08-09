ba5ea4b62f6877837f60ed4940eb36b4989e5df9
Chwdy <1059126322@qq.com>
2016-08-09 20:14:41 +0800

    底栏以及微博修改

--- footer.php
+++ footer.php
@@ -1,20 +1,24 @@
 <!-- 底部文件开始 -->
 <table width="1280" height="88" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#DE0023">
-  <tr>
 
-    <td width="1144"><table width="1144" border="0" cellspacing="0" cellpadding="0">
         <tr>
-            <td width="500"></td>
-          <td width="150"><img src="<?php bloginfo('template_url'); ?>/images/icons/logo2.png" width="54" height="54" /></td>
-          <td width="993" class="white12">版权所有：共青团东北大学委员会   | 地址：辽宁省沈阳市和平区文化路三巷11号  |  辽ICP备05001360号<br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 设计运维：校团委先锋网络中心&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;投稿邮箱：neuyouthnews@163.com</td>
-        </tr>
-     </table> </td>
+            <td width="120" >
+                <img src="<?php bloginfo('template_url'); ?>/images/wechat-qrcode.png" style="margin: 20px;width: 60px">
+            </td>
+            <td width="1000" class="white12"><p style="margin: 0">版权所有：共青团东北大学委员会　|&nbsp;&nbsp;&nbsp;地址：辽宁省沈阳市和平区文化路三巷11号&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;辽ICP备05001360号</p>
+                 <p style="margin: 0">设计运维：校团委先锋网络中心　　|&nbsp;&nbsp;&nbsp;投稿邮箱：neuyouthnews@163.com</p>
+            </td>
 
-</table>
+          <td width="500" >
+              <img src="<?php bloginfo('template_url'); ?>/images/footlogo.jpg" style="float: right">
+
+          </td>
+        </tr>
+
 <p>
   <!-- 底部文件结束 -->
 </p>
 
+    </table>
 </body>
 </html>
--- index.php
+++ index.php
@@ -168,35 +168,12 @@
     <td height="10"></td>
   </tr>
 </table>
-<table width="300" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td rowspan="2">
-	<!-- 微博类选项开始 -->
-<div class="tabqh3">
-	<div class="menu">
-		<ul>
-			<li class="tabfirstli"><a href="javascript:void(0);" onmouseover="showThis(1,'divNumber','tabContent','tablink');" class="tabactive" id="tablink1">新浪微博</a></li>
-			<li><a href="javascript:void(0);" onmouseover="showThis(2,'divNumber','tabContent','tablink');" id="tablink2">微信公众号</a></li>
-		</ul>
-	</div>
-<table width="300" border="0" cellspacing="0" cellpadding="0" style="padding-top:4px; ">
-  <tr>
-    <td height="2" bgcolor="#D2691E"></td>
-  </tr>
-</table>
-	<div id="tabContent1" name="divNumber" class="tabdiv">
-        <iframe width="100%" height="160" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=160&fansRow=2&ptype=1&speed=0&skin=5&isTitle=0&noborder=1&isWeibo=1&isFans=0&uid=5293007529&verifier=1f9f3b9c&dpc=1"></iframe></div>
-	<div id="tabContent2" name="divNumber" class="tabdiv hiddendiv">
-<table style="background-color: white">
-    <tr width="300px">
-        <td><img src="<?php bloginfo('template_url'); ?>/images/wechat-qrcode.png" width="130px"> </td>
-        <td width="170px"align="center"><p>扫描左侧二维码</p><p>关注微信公众平台</p><p>东北大学团委</p> </td>
-    </tr>
-</table></div>
-</div></td>
-</table>
+<div style="width: 300px;height: 190px">
+        <iframe width="300px" height="160px" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=190&fansRow=2&ptype=1&speed=0&skin=5&isTitle=0&noborder=1&isWeibo=1&isFans=0&uid=5293007529&verifier=1f9f3b9c&dpc=1"></iframe>
+</div>
+
 	<!-- 微博类选项结束 -->
-</td>
+
 <!--左一结束 -->
 <!--中区表格开始 -->
     <td valign="top"><table width="643" border="0" align="center" cellpadding="0" cellspacing="0">
